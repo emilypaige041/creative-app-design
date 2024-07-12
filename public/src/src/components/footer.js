3a1953fbbc3b1213feabd0ca80d0d17222316981
emilypaige041 <emilypaige041@gmail.com>
2024-07-12 12:47:32 -0400

    Create footer.js

---
+++ public/src/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="linkedin">
+      <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+        Connect with me on LinkedIn
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
